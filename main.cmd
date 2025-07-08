fa46d7b2ffceed4f8f6ea9df34a17a2f96d3eb9d
arigi <arigi@arigi.top>
2025-07-08 12:37:05 +0800

    自动导出，显示，复制

--- main.cmd
+++ main.cmd
@@ -1,2 +1,4 @@
-@call conda run --no-capture-output -n blueberry python main.py
-@pause
+@call conda run --no-capture-output -n blueberry python main.py > blueberry.txt
+@type blueberry.txt
+@clip < blueberry.txt
+pause
