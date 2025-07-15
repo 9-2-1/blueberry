d02fb5fc6c31131348cbff1c389b579aa3a2a122
arigi <arigi@arigi.top>
2025-07-15 09:58:16 +0800

    更新时区依赖从pytz到zoneinfo

--- blueberry/config.py
+++ blueberry/config.py
@@ -1,8 +1,8 @@
 from datetime import timedelta
-from pytz import timezone
+from zoneinfo import ZoneInfo
 
 # 提供的时间的时区
-CTZ = timezone("Asia/ShangHai")
+CTZ = ZoneInfo("Asia/Shanghai")
 
 # 8小时(时长) * 80%(工作-休息比) ≈ 6.5小时
 推荐用时 = timedelta(hours=6.5)
