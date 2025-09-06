848913e4e451a1fea8ac0f5a3d57cfa1631c74a5
arigi <arigi@arigi.top>
2025-09-06 18:01:29 +0800

    增加“今日推荐完成”概念（列名写作“计划”）简化fmt函数，goldiechange颜色，空值使用None防止干扰数字判断

--- blueberry/report.py
+++ blueberry/report.py
@@ -1,15 +1,12 @@
-from typing import TypeVar, overload, Optional, Iterable, Union, Literal
+from typing import TypeVar, Optional, Iterable, Union, Literal, Sequence
 from dataclasses import dataclass
 from datetime import datetime, timedelta
-from textwrap import indent
-import warnings
 
 import wcwidth  # type: ignore
 
 # 用于tabulate的宽度计算
 from tabulate import tabulate, SEPARATING_LINE
 
-from .config import 推荐用时
 from .models import AppendOnly, PickerModel
 from .collect import State
 from .statistic import StateStats, isdisabled, EmptyLongTaskStats, EmptyShortTaskStats
@@ -18,82 +15,58 @@ from .statistic import StateStats, isdisabled, EmptyLongTaskStats, EmptyShortTas
 FmtT = TypeVar("FmtT", int, float, datetime, timedelta)
 
 
-@overload
-def fmt(x: FmtT, *, pos: bool = False, p2: bool = False) -> str: ...
-
-
-@overload
 def fmt(
     x: FmtT,
-    y: FmtT,
     *,
     pos: bool = False,
     p2: bool = False,
-    olddiff: bool = True,
-) -> str: ...
-
-
-def fmt(
-    x: FmtT,
-    y: Optional[FmtT] = None,
-    *,
-    pos: bool = False,
-    p2: bool = False,
-    olddiff: bool = True,
 ) -> str:
-    if y is None:
-        if isinstance(x, int):
-            if pos:
-                return f"{x:+d}"
-            else:
-                return f"{x:d}"
-        elif isinstance(x, float):
-            if p2:
-                if pos:
-                    return f"{x:+.2f}"
-                else:
-                    return f"{x:.2f}"
-            else:
-                if pos:
-                    return f"{x:+g}"
-                else:
-                    return f"{x:g}"
-        elif isinstance(x, datetime):
-            if pos:
-                raise TypeError("datetime无符号")
-            else:
-                return x.strftime("%m/%d %H:%M")
-        elif isinstance(x, timedelta):
-            sign = ""
-            if x >= timedelta(0):
-                if pos:
-                    sign = "+"
-                v = x
-            else:
-                sign = "-"
-                v = -x
-            if v > timedelta(days=7):
-                vstr = f"{v // timedelta(days=1)}d"
-            elif v > timedelta(days=1):
-                vstr = f"{v // timedelta(days=1)}d{v // timedelta(hours=1) % 24}h"
-            elif v > timedelta(hours=1):
-                vstr = f"{v // timedelta(hours=1)}h{v // timedelta(minutes=1) % 60}m"
-            else:
-                vstr = f"{v.seconds // 60}min"
-            return sign + vstr
+    if isinstance(x, int):
+        if pos and x != 0:
+            return f"{x:+d}"
         else:
-            raise TypeError("未知类型")
+            return f"{x:d}"
+    elif isinstance(x, float):
+        if p2:
+            if pos and x != 0.0:
+                return f"{x:+.2f}"
+            else:
+                return f"{x:.2f}"
+        else:
+            if pos and x != 0.0:
+                return f"{x:+g}"
+            else:
+                return f"{x:g}"
+    elif isinstance(x, datetime):
+        if pos:
+            raise TypeError("datetime无符号")
+        else:
+            return x.strftime("%m/%d %H:%M")
+    elif isinstance(x, timedelta):
+        sign = ""
+        if x >= timedelta(0):
+            if pos and x != timedelta(0):
+                sign = "+"
+            v = x
+        else:
+            sign = "-"
+            v = -x
+        if v > timedelta(days=7):
+            vstr = f"{v // timedelta(days=1)}d"
+        elif v > timedelta(days=1):
+            vstr = f"{v // timedelta(days=1)}d{v // timedelta(hours=1) % 24}h"
+        elif v > timedelta(hours=1):
+            vstr = f"{v // timedelta(hours=1)}h{v // timedelta(minutes=1) % 60}m"
+        elif v > timedelta(minutes=1):
+            vstr = f"{v // timedelta(minutes=1)}min"
+        else:
+            vstr = "0"
+        return sign + vstr
     else:
-        if olddiff:
-            return f"{fmt(x, pos=pos)} → {fmt(y, pos=pos)}"
-        else:
-            if isinstance(y, timedelta):
-                return f"{fmt(y, pos=pos)}({fmt(y - x, pos=True)})"
-            else:
-                return f"{fmt(y, pos=pos)}({fmt(y - x, pos=True)})"
+        raise TypeError("未知类型")
 
 
-ColorMode = Literal["goldie", "shadowzero"]
+ColorMode = Literal["goldie", "shadowzero", "goldiechange"]
 
 
 def colorit(value: FmtT, fmtstr: str, colormode: ColorMode) -> str:
@@ -105,6 +78,11 @@ def colorit(value: FmtT, fmtstr: str, colormode: ColorMode) -> str:
     elif colormode == "shadowzero":
         if value in {0, timedelta(0)}:
             fmtstr = f"{ESC}[{DARKGREY}m{fmtstr}{ESC}[0m"
+        return fmtstr
+    elif colormode == "goldiechange":
+        assert isinstance(value, int)
+        color = goldie_change_color(value)
+        fmtstr = f"{ESC}[{color}m{fmtstr}{ESC}[0m"
         return fmtstr
     else:
         raise ValueError(f"未知颜色模式: {colormode}")
@@ -136,6 +114,17 @@ def goldie_color(goldie: int) -> str:
     return goldie_levels[-1]
 
 
+goldie_change_thresholds = [-50, -20, 0, 1, 20, 50]
+goldie_change_levels = [RED, ORANGE, YELLOW, DARKGREY, GREEN, CYAN, BLUE]
+
+
+def goldie_change_color(goldie: int) -> str:
+    for i, threshold in enumerate(goldie_change_thresholds):
+        if goldie < threshold:
+            return goldie_change_levels[i]
+    return goldie_change_levels[-1]
+
+
 @dataclass
 class ReportData:
     time: datetime
@@ -163,7 +152,7 @@ def report_head(N: ReportData) -> str:
     report = f"blueberry - {N.time.strftime('%Y-%m-%d %H:%M:%S')}\n"
     report += f"近期每日平均用时: {fmt(N.stats.总每日平均用时)}\n"
     report += f"建议每日用时: {fmt(N.stats.建议每日用时)} "
-    report += f"(在 {fmt(N.time, N.stats.下一关键时间, olddiff=False)}"
+    report += f"(在 {fmt(N.stats.下一关键时间)} ({fmt(N.stats.下一关键时间 - N.time)})"
     report += f" 前完成 {fmt(N.stats.下一关键节点任务量时长)} 工作量)"
     return report
 
@@ -193,13 +182,13 @@ def report_long_tasks(N: ReportData) -> str:
     ]
     推荐每日用时 = timedelta(0)
     总预计时间 = timedelta(0)
-    table_lines: list[Union[list[str], str]] = []
+    table_lines: list[Union[Sequence[Optional[str]], str]] = []
     for task in prefer(N.state.长期任务.values(), N.state.选择排序偏好):
         tstat = N.stats.长期任务统计[task.名称]
         # 跳过完成0分项
         if tstat.进度 >= task.总数 and N.time >= task.最晚结束:
             continue
-        # ["", "名称", "|", "点数", "推荐时长", "完成", "剩余", "剩余时间", "预计用时", "每日平均用时"]
+        # [None, "名称", "|", "点数", "推荐时长", "完成", "剩余", "剩余时间", "预计用时", "每日平均用时"]
         colorpts = tstat.点数 - (0 if tstat.进度 > 0 else 1)
         table_line = [
             colorit(
@@ -224,14 +213,14 @@ def report_long_tasks(N: ReportData) -> str:
         table_lines.append(table_line)
     table_lines.append(SEPARATING_LINE)
     total_line = [
-        "",
+        None,
         "总数",
         "|",
         colorit(N.stats.长期任务点数, fmt(N.stats.长期任务点数), "goldie"),
         colorit(推荐每日用时, fmt(推荐每日用时), "shadowzero"),
-        "",
-        "",
-        "",
+        None,
+        None,
+        None,
         colorit(总预计时间, fmt(总预计时间), "shadowzero"),
         colorit(N.stats.总每日平均用时, fmt(N.stats.总每日平均用时), "shadowzero"),
     ]
@@ -241,7 +230,7 @@ def report_long_tasks(N: ReportData) -> str:
 
 
 def report_short_tasks(N: ReportData) -> str:
-    table_lines: list[Union[list[str], str]] = []
+    table_lines: list[Union[Sequence[Optional[str]], str]] = []
     table_headers = [
         "",
         "短期任务",
@@ -281,14 +270,14 @@ def report_short_tasks(N: ReportData) -> str:
         table_lines.append(table_line)
     table_lines.append(SEPARATING_LINE)
     total_line = [
-        "",
+        None,
         "总数",
         "|",
         colorit(N.stats.短期任务点数, fmt(N.stats.短期任务点数), "goldie"),
         colorit(推荐每日用时, fmt(推荐每日用时), "shadowzero"),
         colorit(总用时, fmt(总用时), "shadowzero"),
         colorit(总预计时间, fmt(总预计时间), "shadowzero"),
-        "",
+        None,
     ]
     table_lines.append(total_line)
     report = tabulate(table_lines, headers=table_headers, tablefmt="simple")
@@ -304,14 +293,16 @@ def report_tasks_diff(N: ReportData, P: ReportData, hide_decay: bool = False) ->
         "完成",
         "点数",
         "变化",
-        "推荐时长",
+        "推荐",
+        "预计",
         "剩余时间",
     ]
     推荐每日用时 = timedelta(0)
     总用时 = timedelta(0)
     其它点数 = 0
     其它点数变化 = 0
-    table_lines: list[Union[list[str], str]] = []
+    其它推荐每日用时 = timedelta(0)
+    table_lines: list[Union[Sequence[Optional[str]], str]] = []
     for ntask1 in prefer(N.state.长期任务.values(), N.state.选择排序偏好):
         nstat1 = N.stats.长期任务统计[ntask1.名称]
         if ntask1.名称 not in P.stats.长期任务统计:
@@ -325,11 +316,13 @@ def report_tasks_diff(N: ReportData, P: ReportData, hide_decay: bool = False) ->
         if hide_decay and nstat1.用时 == pstat1.用时:
             其它点数 += nstat1.点数
             其它点数变化 += nstat1.点数 - pstat1.点数
+            其它推荐每日用时 += nstat1.推荐每日用时
             continue
-        # ["", "名称", "|", "用时", "完成", "点数", "变化", "推荐时长", "剩余时间"]
+        # [None, "名称", "|", "用时", "完成", "点数", "变化", "推荐", "预计", "剩余时间"]
         colorpts = nstat1.点数 - (0 if nstat1.进度 > 0 else 1)
         reach_recommend = nstat1.用时 - pstat1.用时 >= nstat1.推荐每日用时
-        table_line = [
+        推荐完成 = nstat1.推荐每日用时 / timedelta(hours=1) * nstat1.速度
+        table_line: list[Optional[str]] = [
             colorit(
                 colorpts,
                 LONG_RUNNING if nstat1.用时 != pstat1.用时 else LONG_WAITING,
@@ -345,8 +338,11 @@ def report_tasks_diff(N: ReportData, P: ReportData, hide_decay: bool = False) ->
             ),
             colorit(colorpts, fmt(nstat1.点数), "goldie"),
             colorit(
-                nstat1.点数 - pstat1.点数, fmt(nstat1.点数 - pstat1.点数), "shadowzero"
+                nstat1.点数 - pstat1.点数,
+                fmt(nstat1.点数 - pstat1.点数, pos=True),
+                "goldiechange",
             ),
+            colorit(推荐完成, fmt(推荐完成, p2=True), "shadowzero"),
             colorit(nstat1.推荐每日用时, fmt(nstat1.推荐每日用时), "shadowzero"),
             (
                 fmt(ntask1.最晚结束 - N.time)
@@ -373,8 +369,9 @@ def report_tasks_diff(N: ReportData, P: ReportData, hide_decay: bool = False) ->
         if hide_decay and nstat2.用时 == pstat2.用时:
             其它点数 += nstat2.点数
             其它点数变化 += nstat2.点数 - pstat2.点数
+            其它推荐每日用时 += nstat2.推荐每日用时
             continue
-        # ["", "名称", "|", "用时", "完成", "点数", "变化", "推荐时长", "剩余时间"]
+        # [None, "名称", "|", "用时", "完成", "点数", "变化", "推荐", "预计", "剩余时间"]
         colorpts = nstat2.点数 - (0 if ntask2.完成 is not None else 1)
         reach_recommend = ntask2.完成 is not None or nstat2.推荐每日用时 == timedelta(0)
         table_line = [
@@ -388,11 +385,14 @@ def report_tasks_diff(N: ReportData, P: ReportData, hide_decay: bool = False) ->
             colorit(
                 nstat2.用时 - pstat2.用时, fmt(nstat2.用时 - pstat2.用时), "shadowzero"
             ),
-            "√" if ntask2.完成 is not None else "",
+            "√" if ntask2.完成 is not None else None,
             colorit(colorpts, fmt(nstat2.点数), "goldie"),
             colorit(
-                nstat2.点数 - pstat2.点数, fmt(nstat2.点数 - pstat2.点数), "shadowzero"
+                nstat2.点数 - pstat2.点数,
+                fmt(nstat2.点数 - pstat2.点数, pos=True),
+                "goldiechange",
             ),
+            None,
             colorit(nstat2.推荐每日用时, fmt(nstat2.推荐每日用时), "shadowzero"),
             fmt(ntask2.最晚结束 - N.time),
         ]
@@ -401,31 +401,33 @@ def report_tasks_diff(N: ReportData, P: ReportData, hide_decay: bool = False) ->
     table_lines.append(SEPARATING_LINE)
     if hide_decay:
         total_line = [
-            "",
+            None,
             "其它",
             "|",
-            "",
-            "",
+            None,
+            None,
             colorit(其它点数, fmt(其它点数), "goldie"),
-            colorit(其它点数变化, fmt(其它点数变化), "shadowzero"),
-            "",
-            "",
+            colorit(其它点数变化, fmt(其它点数变化), "goldiechange"),
+            None,
+            colorit(其它推荐每日用时, fmt(其它推荐每日用时), "goldiechange"),
+            None,
         ]
         table_lines.append(total_line)
     total_line = [
-        "",
+        None,
         "总数",
         "|",
         colorit(总用时, fmt(总用时), "shadowzero"),
-        "",
+        None,
         colorit(N.stats.Goldie点数, fmt(N.stats.Goldie点数), "goldie"),
         colorit(
             N.stats.Goldie点数 - P.stats.Goldie点数,
-            fmt(N.stats.Goldie点数 - P.stats.Goldie点数),
-            "shadowzero",
+            fmt(N.stats.Goldie点数 - P.stats.Goldie点数, pos=True),
+            "goldiechange",
         ),
+        None,
         colorit(推荐每日用时, fmt(推荐每日用时), "shadowzero"),
-        "",
+        None,
     ]
     table_lines.append(total_line)
     report = tabulate(table_lines, headers=table_headers, tablefmt="simple")
