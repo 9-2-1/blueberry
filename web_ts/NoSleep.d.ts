e16ca230af6bf9cf3ddb31815ae7c00750dc0a7e
arigi <arigi@arigi.top>
2025-10-11 14:52:10 +0800

    TypeScript

---
+++ web_ts/NoSleep.d.ts
@@ -0,0 +1,4 @@
+class NoSleep {
+  enable(): void;
+  disable(): void;
+}
--- web_ts/script.ts
+++ web_ts/script.ts
@@ -9,9 +9,21 @@ const colorMap = {
   default: "rgb(212, 212, 212)",
 };
 
+const appDiv = document.getElementById("app") as HTMLElement;
+const pointsDiv = document.getElementById("points") as HTMLElement;
+const nowtimeDiv = document.getElementById("nowtime") as HTMLElement;
+const nowdateDiv = document.getElementById("nowdate") as HTMLElement;
+
 function updatePoints() {
   const point_thresholds = [-100, -50, 0, 50, 100];
-  const rating_lists = ["D", "C", "B", "A", "AA", "AAA"];
+  const rating_lists: (keyof typeof colorMap)[] = [
+    "D",
+    "C",
+    "B",
+    "A",
+    "AA",
+    "AAA",
+  ];
   fetch("get_points", { method: "get" })
     .then((response) => {
       if (response.status != 200) {
@@ -20,20 +32,20 @@ function updatePoints() {
       return response.text();
     })
     .then((points) => {
-      document.getElementById("points").innerText = points;
-      let rating = "D";
+      pointsDiv.innerText = points;
+      let rating: keyof typeof colorMap = "D";
       for (const [i, x] of point_thresholds.entries()) {
-        if (points >= x) {
+        if (Number(points) >= x) {
           rating = rating_lists[i + 1];
         }
       }
-      document.getElementById("app").style.color = colorMap[rating];
+      appDiv.style.color = colorMap[rating];
       setTimeout(updatePoints, 10000);
     })
     .catch((error) => {
       console.error(error);
-      document.getElementById("app").style.color = colorMap["default"];
-      document.getElementById("points").innerText = "----";
+      appDiv.style.color = colorMap["default"];
+      pointsDiv.innerText = "----";
       setTimeout(updatePoints, 10000);
     });
 }
@@ -45,14 +57,12 @@ function updateTime() {
   let hours = now.getHours();
   let minutes = now.getMinutes();
   let seconds = now.getSeconds();
-  document.getElementById("nowtime").innerText =
-    `${hours.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+  nowtimeDiv.innerText = `${hours.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
   let day = now.getDate();
   let month = now.getMonth() + 1;
   let weekday = now.getDay();
   let week = ["日", "一", "二", "三", "四", "五", "六"];
-  document.getElementById("nowdate").innerText =
-    `${month}/${day} ${week[weekday]}`;
+  nowdateDiv.innerText = `${month}/${day} ${week[weekday]}`;
   setTimeout(updateTime, 1000 - now.getMilliseconds());
 }
 
@@ -62,8 +72,8 @@ window.onload = function () {
   const nosleep = new NoSleep();
 
   document.body.onclick = function () {
-    document.getElementById("app").classList.toggle("layout-points");
-    document.getElementById("app").classList.toggle("layout-time");
+    appDiv.classList.toggle("layout-points");
+    appDiv.classList.toggle("layout-time");
   };
 
   document.body.oncontextmenu = function (e) {
@@ -80,5 +90,5 @@ window.onload = function () {
 
   // font after script
   // new FontFace("Fira Mono", "url(FiraMono.ttf)");
-  document.getElementById("app").classList.add("load-font");
+  appDiv.classList.add("load-font");
 };
